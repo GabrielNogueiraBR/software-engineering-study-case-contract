cfc8e381d5151e13074beccb18dbc0b7073f24c0
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2023-04-12 19:45:09 -0300

    chore: setup project with Java Maven and Spring Boot

---
+++ src/test/java/com/example/contratacao/ContratacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contratacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
