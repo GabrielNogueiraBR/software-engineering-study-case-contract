9c7cdcf5c0875b0abe86f0d31ff57a2f6b2d8b3c
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2023-04-12 20:02:17 -0300

    feat: adding class Candidato

---
+++ src/main/java/contratacao/Candidato.java
@@ -0,0 +1,22 @@
+package contratacao;
+
+public class Candidato {
+    private int idade;
+
+    public Candidato(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean contratarCandidato() {
+    	if(idade < 16) {
+    		return false;
+    	}
+    	else if (idade > 55) {
+            return false;
+        } else if (idade >= 16 && idade <= 18) {
+            return true; // contratação em tempo parcial
+        } else {
+            return true; // contratação em período integral
+        }
+    }
+}
